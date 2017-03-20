9a2252dc271b34130ffe9def286d4fc6e5f617c5
sitecoreguild <chris.williams@readwatchcreate.com>
2017-03-20 17:52:26 -0400

    Initial Deploy of CSHARP.Data.SqlServer.Standard. Also moved Solution file out of CSHARP.Data.

---
+++ Apps/CSHARP.Data.SqlServer.Standard/SqlParameters.cs
@@ -0,0 +1,48 @@
+﻿/********************************************************************************
+ * CSHARP SQL Library - General Elements used to work with SQL Server
+ * 
+ * NOTE: Adapted from Clinch.Data
+ * 
+ * LICENSE: Free to use provided details on fixes and/or extensions emailed to 
+ *          chris.williams@readwatchcreate.com
+ ********************************************************************************/
+
+ using System.Collections.Generic;
+
+namespace CSHARP.Data.SQL.Standard
+{
+    /// <summary>
+    /// Encapsulates set of parameters to pass to stored procedure
+    /// </summary>
+    public class SqlParameters
+    {
+        /// <summary>
+        /// List of all the parameter names
+        /// </summary>
+        public List<string> ParameterNames = new List<string>();
+
+        /// <summary>
+        /// List of all the parameter values
+        /// </summary>
+        public List<string> ParameterValues = new List<string>();
+
+        /// <summary>
+        /// List of all the parameter types
+        /// </summary>
+        public List<string> ParameterTypes = new List<string>();
+
+        /// <summary>
+        /// Adds a parameter to the lists
+        /// </summary>
+        /// <param name="name">Name of the parameter</param>
+        /// <param name="value">Value for the parameter</param>
+        /// <param name="type">Data type of the parameter</param>
+        /// <returns></returns>
+        public void AppendParameter(string name, string value, string type)
+        {
+            ParameterNames.Add(name);
+            ParameterValues.Add(value);
+            ParameterTypes.Add(type);
+        }
+    }
+}
